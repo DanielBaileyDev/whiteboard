4a2f656bf829e638b3d10eb95a95872eb35433a2
Daniel <daniel.bailey.nsw@gmail.com>
2022-12-08 12:56:37 +1100

    fixed errors with database that would cause server to stop, boards delete 24hrs after creation

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
-const PORT = 8800;
-
 const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,44 +16,56 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-client.connect(err => {
-    const collection = client.db("whiteboard").collection("boards");
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    });
+const collection = client.db("whiteboard").collection("boards");
+collection.createIndex({ "createdAt": 1 }, { expireAfterSeconds: 86400 });
 
-    app.get('/whiteboard/:board', (req, res) => {
-        res.sendFile(__dirname + '/whiteboard.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-    });
+app.get('/whiteboard/:board', (req, res) => {
+    res.sendFile(__dirname + '/whiteboard.html');
 
-    app.get('/createboard', (req, res) => {
-        collection.insertOne({ lines: [] })
-            .then(result => {
-                res.send(result.insertedId);
-            });
-    });
+});
 
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-        let boardName = socket.handshake.headers.referer.split('/').at(-1);
+app.get('/createboard', (req, res) => {
+    collection.insertOne({
+        "createdAt": new Date(),
+        lines: []
+    })
+        .then(result => {
+            res.send(result.insertedId);
+        });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    let boardName = socket.handshake.headers.referer.split('/').at(-1);
+    if (ObjectId.isValid(boardName)) {
         socket.join(boardName);
-        // Create catch here for when user connects to a board that doesn't exist
         collection.findOne({ _id: ObjectId(boardName) })
             .then(result => {
                 socket.emit('board', result.lines);
+            })
+            .catch((error) => {
+                console.error(error);
             });
+    }
 
-        socket.on('board', (board) => {
-            collection.updateOne({ _id: ObjectId(board.name) }, { $push: { lines: board.line } });
+    socket.on('board', (board) => {
+        if (ObjectId.isValid(board.name)) {
+            collection.updateOne({ _id: ObjectId(board.name) }, { $push: { lines: board.line } })
+                .catch((error) => {
+                    console.error(error);
+                });
             socket.broadcast.to(board.name).emit('board', [board.line]);
-        });
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
+        }
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
+server.listen(process.env.PORT, () => {
+    console.log(`listening on *:${process.env.PORT}`);
 });
